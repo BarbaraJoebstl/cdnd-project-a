545f10efb8a2f8acaa30732e481869afdb4fe24b
Barbara Jöbstl <Barbara.Joebstl@mercedes-benz.io>
2019-12-10 18:50:47 +0100

    fix: start script

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-import { IndexRouter } from './controllers/v0/index.router';
+import { Request, Response } from 'express';
+import { filterImageFromURL, deleteLocalFiles } from './util/util';
 
 (async () => {
 
@@ -13,15 +14,37 @@ import { IndexRouter } from './controllers/v0/index.router';
   // Use the body parser middleware for post requests
   app.use(bodyParser.json());
 
-  //CORS Should be restricted
-  app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "http://localhost:8100");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-    next();
-  });
+  //  app.use('/', IndexRouter);
 
-  app.use('/', IndexRouter);
 
+  app.get("/", async(req: Request, res: Response) => {
+    res.send("try /filteredimage?image_url={{anyimageurl}}")
+  })
+
+  /**
+  * filters an image url and returns the image file
+  */
+  app.get("/filteredimage", async(req: Request, res: Response) => {
+  const imageUrl = req.param('image_url');
+
+  if (!imageUrl) {
+    return res.status(400).send(`please provide an url`);
+    }
+
+  // call filteredImageFromURL(img_url) to filter the image
+  const filteredPath = await filterImageFromURL(imageUrl);
+
+  if (!filteredPath) {
+    return res.status(422).send(`we can t find an image.`);
+  }
+
+  // send the resulting file in the response
+  res.status(200).sendFile(filteredPath);
+
+  // delete file on server
+  // await deleteLocalFiles([filteredPath]);
+
+  })
 
   // Start the Server
   app.listen( port, () => {
